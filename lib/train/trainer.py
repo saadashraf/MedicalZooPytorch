4fdd9a06165d161a9d8c19b18a3e37bed9ef41b7
saadbinashraf <saadbinashraf14@gmail.com>
2025-04-08 11:57:48 -0600

    Modified trainer file for added features - add early stopping, filename for better tracking.

--- lib/train/trainer.py
+++ lib/train/trainer.py
@@ -1,6 +1,11 @@
+from typing import Optional
+
+import os
 import numpy as np
 import torch
+import nibabel as nib
 
+from lib.utils.early_stopping import EarlyStopping
 from lib.utils.general import prepare_input
 from lib.visual3D_temp.BaseWriter import TensorboardWriter
 
@@ -10,14 +15,23 @@ class Trainer:
     Trainer class
     """
 
-    def __init__(self, args, model, criterion, optimizer, train_data_loader,
-                 valid_data_loader=None, lr_scheduler=None):
+    def __init__(self, args, model, criterion, optimizer, train_data_loader, valid_data_loader=None, lr_scheduler=None):
 
         self.args = args
         self.model = model
         self.optimizer = optimizer
         self.criterion = criterion
         self.train_data_loader = train_data_loader
+
+        self.early_stopping_patience = args.early_stopping_patience
+
+        self.early_stopping = (
+            EarlyStopping(patience=self.early_stopping_patience)
+            if self.early_stopping_patience
+            else None
+        )
+
+
         # epoch-based training
         self.len_epoch = len(self.train_data_loader)
         self.valid_data_loader = valid_data_loader
@@ -49,12 +63,22 @@ class Trainer:
             self.writer.reset('train')
             self.writer.reset('val')
 
+            if self.early_stopping:
+                self.early_stopping(val_loss=val_loss)
+
+                if self.early_stopping.early_stop:
+                    print("Early stopping")
+                    break
+
     def train_epoch(self, epoch):
         self.model.train()
 
-        for batch_idx, input_tuple in enumerate(self.train_data_loader):
+        for batch_idx, inputs in enumerate(self.train_data_loader):
 
             self.optimizer.zero_grad()
+
+            input_tuple = inputs[:-1]
+            input_filename = inputs[-1]
 
             input_tensor, target = prepare_input(input_tuple=input_tuple, args=self.args)
             input_tensor.requires_grad = True
@@ -81,9 +105,23 @@ class Trainer:
                 input_tensor.requires_grad = False
 
                 output = self.model(input_tensor)
+
+                if epoch % 5 == 0:
+                    self.save_images(output[0], str(epoch))
+
                 loss, per_ch_score = self.criterion(output, target)
 
                 self.writer.update_scores(batch_idx, loss.item(), per_ch_score, 'val',
                                           epoch * self.len_epoch + batch_idx)
 
         self.writer.display_terminal(len(self.valid_data_loader), epoch, mode='val', summary=True)
+
+    def save_images(self, image, name):
+        print(f"Now saving image: {image}")
+        output_path = "results/output_images"
+
+        for index in range(image.shape[0]):
+            slice = image[index].detach().cpu().numpy()
+            slice = slice.squeeze()
+            nib_image = nib.Nifti1Image(slice, affine=np.eye(4))
+            nib.save(nib_image, os.path.join(output_path, name + f"_{index}.nii"))
